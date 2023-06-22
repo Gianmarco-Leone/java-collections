9581dacf52f00da7ac0d51928f2eef5389082c6a
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-06-22 17:15:59 +0200

    add ex2

---
+++ src/main/java/com/gianmarco/collections/Exercise2.java
@@ -0,0 +1,29 @@
+//  Same as exercise 1 except we don’t care about retaining the order and we want to ensure
+//  that duplicates will not exist
+
+package com.gianmarco.collections;
+
+import java.time.Year;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Exercise2 {
+    // Invece di creare un nuovo record qui potrei usare lo stesso dell'ex1
+    public record Car(String brand, String model, Year productionYear) {}
+
+    public static void main(String[] args) {
+        Set<Car> cars = new HashSet<>();
+        cars.add(new Car("VW", "Polo", Year.of(2019)));
+        cars.add(new Car("FIAT", "Punto", Year.of(2009)));
+        cars.add(new Car("FIAT", "Punto", Year.of(2009))); // duplicate
+        cars.add(new Car("FORD", "Puma", Year.of(2020)));
+        cars.add(new Car("FORD", "Puma", Year.of(2020))); // duplicate
+        cars.add(new Car("TOYOTA", "Yaris", Year.of(2012)));
+        cars.add(new Car("TOYOTA", "Yaris", Year.of(2012))); // duplicate
+        cars.add(new Car("MINI", "One", Year.of(2006)));
+
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+    }
+}
